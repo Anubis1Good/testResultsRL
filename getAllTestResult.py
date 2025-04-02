e90b137dd69cd0355c2d3b9fc77e1fbc637d51ef
Anubis1Good <vlad.polyakov.96@inbox.ru>
2025-04-02 23:45:03 +0300

    mb last commit

--- getAllTestResult.py
+++ getAllTestResult.py
@@ -4,7 +4,7 @@ import pandas as pd
 parent_folder = 'all_test_result'
 raw_folder = 'all_test_results'
 prefix = '_moex'
-prefix = '_bitget'
+# prefix = '_bitget'
 raw_folder += prefix
 raw_folder = os.path.join(parent_folder,raw_folder)
 raw_files = os.listdir(raw_folder)
